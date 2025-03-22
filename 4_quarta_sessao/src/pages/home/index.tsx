430d0842e85ec017696f03baebf769da7a219b30
luisitcho <luishenrisc1@gmail.com>
2025-03-22 20:05:24 -0300

    curso react - projeto linktree + firebase

--- 4_quarta_sessao/src/pages/home/index.tsx
+++ 4_quarta_sessao/src/pages/home/index.tsx
@@ -1,7 +1,23 @@
 export function Home() {
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <div className="flex flex-col items-center justify-center h-screen w-full">
+            <h1 className="xl:text-4xl text-3xl font-bold text-white mt-20">Luisitcho</h1>
+            <span className="text-gray-50 mb-5 mt-3">Veja os links</span>
+
+            <main className="main flex flex-col w-11/12 max-w-xl text-center">
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+                    <a>
+                        <p className="text-base xl:text-xl">
+                            Link 1
+                        </p>
+                    </a>
+                </section>
+
+                <footer className="footer fkex justify-center items-center gap-3 my-4">
+
+                </footer>
+
+            </main>
+        </div>
     )
 }
