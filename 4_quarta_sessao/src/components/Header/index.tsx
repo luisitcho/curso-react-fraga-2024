9ba9f8dbf46c264fb7dacb0fa4c3687bac492936
luisitcho <luishenrisc1@gmail.com>
2025-08-10 22:31:05 -0300

    curso react - projeto linktree + firebase | header

--- 4_quarta_sessao/src/components/Header/index.tsx
+++ 4_quarta_sessao/src/components/Header/index.tsx
@@ -1,12 +1,12 @@
-import { BiLogOut } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
-import { auth } from '../../services/firebaseConnection';
-import { signOut } from 'firebase/auth';
+import { BiLogOut } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { auth } from "../../services/firebaseConnection";
+import { signOut } from "firebase/auth";
 export function Header() {
     async function handleLogout() {
         await signOut(auth);
     }
-    
+
     return (
         <header className="header w-full max-w-2xl mt-4 px-1">
             <nav className="w-full bg-white h-12 flex items-center justify-between rounded-md px-3">
@@ -15,10 +15,10 @@ export function Header() {
                     <Link to="/admin">Admin</Link>
                     <Link to="/admin/networks">Redes Sociais</Link>
                 </div>
-                <button onClick={handleLogout}>
+                <button onClick={handleLogout} className="cursor-pointer">
                     <BiLogOut size={28} color="#db2629" />
                 </button>
             </nav>
         </header>
-    )
+    );
 }
