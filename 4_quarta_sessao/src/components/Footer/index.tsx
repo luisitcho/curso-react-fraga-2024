b15317052aa94539501a86d27ec9939383899d83
luisitcho <luishenrisc1@gmail.com>
2025-08-10 22:05:05 -0300

    curso react - projeto linktree + firebase | Finalizando página home

--- 4_quarta_sessao/src/components/Footer/index.tsx
+++ 4_quarta_sessao/src/components/Footer/index.tsx
@@ -1,12 +1,61 @@
+import { useEffect, useState } from "react";
 import { Social } from "../../components/Social";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+
+type socialLinksProps = {
+    linkedin: string;
+    github: string;
+    instagram: string;
+};
 
 export function Footer() {
+    const [socialLinks, setSocialLinks] = useState<socialLinksProps>();
+
+    useEffect(() => {
+        (function socialLinks() {
+            const docRef = doc(db, "social", "link");
+
+            getDoc(docRef)
+                .then((snapshot) => {
+                    const data = snapshot.data();
+
+                    if (data) {
+                        setSocialLinks({
+                            linkedin: data.linkedin ?? "",
+                            github: data.github ?? "",
+                            instagram: data.instagram ?? "",
+                        });
+                    }
+                })
+                .catch((error) => {
+                    console.log("Erro ao recupertar os dados", error);
+                });
+        })();
+    }, []);
+
+    const media = {
+        linkedin: FaLinkedin,
+        github: FaGithub,
+        instagram: FaInstagram,
+    };
+
     return (
-        <footer className="footer flex justify-center items-center gap-3 my-4">
-            <Social url="https://www.linkedin.com/in/luishenriquesc/" icon={FaLinkedin} />
-            <Social url="https://github.com/luisitcho/" icon={FaGithub} />
-            <Social url="https://www.instagram.com/luisitcho/" icon={FaInstagram} />
-        </footer>
-    )
+        <>
+            {socialLinks && Object.keys(socialLinks).length > 0 && (
+                <footer className="footer flex justify-center items-center gap-3 my-4">
+                    {Object.entries(socialLinks).map(([key, url]) =>
+                        url ? (
+                            <Social
+                                key={key}
+                                url={url}
+                                icon={media[key as keyof typeof media]}
+                            />
+                        ) : null
+                    )}
+                </footer>
+            )}
+        </>
+    );
 }
--- 4_quarta_sessao/src/pages/home/index.tsx
+++ 4_quarta_sessao/src/pages/home/index.tsx
@@ -1,22 +1,80 @@
+import { useEffect, useState } from "react";
 import { Footer } from "../../components/Footer";
+import { db } from "../../services/firebaseConnection";
+import {
+    getDocs,
+    collection,
+    orderBy,
+    query,
+} from "firebase/firestore";
+
+type linkProps = {
+    id: string;
+    name: string;
+    link: string;
+    color: string;
+    background: string;
+};
 
 export function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen w-full">
-      <h1 className="xl:text-4xl text-3xl font-bold text-white mt-20">
-        Luisitcho
-      </h1>
-      <span className="text-gray-50 mb-5 mt-3">Veja os links</span>
+    const [links, setLinks] = useState<linkProps[]>([]);
 
-      <main className="main flex flex-col w-11/12 max-w-xl text-center">
-        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
-          <a>
-            <p className="text-base xl:text-xl">Link 1</p>
-          </a>
-        </section>
+    useEffect(() => {
+        (function loadLinks() {
+            const linksRef = collection(db, "links");
+            const queryRef = query(linksRef, orderBy("created", "desc"));
 
-        <Footer />
-      </main>
-    </div>
-  );
+            getDocs(queryRef)
+                .then((snapshot) => {
+                    const list: linkProps[] = [];
+                    snapshot.forEach(function (doc) {
+                        list.push({
+                            id: doc.id,
+                            name: doc.data().name,
+                            link: doc.data().link,
+                            color: doc.data().color,
+                            background: doc.data().background,
+                        });
+                    });
+
+                    setLinks(list);
+                })
+                .catch((error) => {
+                    console.log("Erro ao recupertar os dados", error);
+                });
+        })();
+    }, []);
+
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen w-full">
+            <h1 className="xl:text-4xl text-3xl font-bold text-white mt-20">
+                Luisitcho
+            </h1>
+            <span className="text-gray-50 mb-5 mt-3">Veja os links</span>
+
+            <main className="main flex flex-col w-11/12 max-w-xl text-center">
+                {links.map((link) => {
+                    return (
+                        <section
+                            key={link.id}
+                            className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer"
+                            style={{ backgroundColor: link.background }}
+                        >
+                            <a href={link.link} target="_blank">
+                                <p
+                                    className="text-base xl:text-xl"
+                                    style={{ color: link.color }}
+                                >
+                                    {link.name}
+                                </p>
+                            </a>
+                        </section>
+                    );
+                })}
+
+                <Footer/>
+            </main>
+        </div>
+    );
 }
