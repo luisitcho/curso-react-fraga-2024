6e2ac62f9a442ab6019e37e0d57f0ff1c7ff77c3
luisitcho <luishenrisc1@gmail.com>
2025-08-15 00:20:27 -0300

    curso react - projeto e-commerce | header

--- 6_sexta_secao_e_commerce/src/components/header/index.tsx
+++ 6_sexta_secao_e_commerce/src/components/header/index.tsx
@@ -1,8 +1,21 @@
+import { FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
 export function Header() {
     return (
-        <header className="header">
+        <header className="header bg-gray-500">
             <div className="container">
-                <h1>Header</h1>
+                <nav className="flex items-center justify-between h-14">
+                    <Link to={"/"} className="font-bold text-2xl">
+                        Dev Shop
+                    </Link>
+                    <Link to={"/cart"} className="relative">
+                        <FiShoppingCart size={24} color="#121212" />
+                        <span className="absolute -top-3 -right-3 px-1.5 bg-sky-500 rounded-full w-4 h-4 flex items-center justify-center text-white text-xs">
+                            2
+                        </span>
+                    </Link>
+                </nav>
             </div>
         </header>
     );
